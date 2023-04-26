adf8ed43469bfa8b6f272761d78cb5ad7e4c5152
93amr.kakashi <93amr.kakashi@gmail.com>
2023-04-27 03:26:49 +0700

    Pwa

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,20 @@ function App() {
     setUserAnswer("");
     fetchQuestion();
   };
+  // const requestNotificationPermission = async () => {
+  //   try {
+  //     const permission = await Notification.requestPermission();
+  //     if (permission !== "granted") {
+  //       throw new Error("Permission not granted for Notifications");
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
+  // useEffect(() => {
+  //   requestNotificationPermission()
+  // },[])
   return (
     <div className={isDarkMode ? "App dark-mode" : "App light-mode"}>
       {/* navbar component */}
